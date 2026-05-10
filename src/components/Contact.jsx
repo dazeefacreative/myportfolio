580f4d7315e64b4d5e0e19a10e2b38dac17964bf
Afeez Lawal <dazeefa@gmail.com>
2026-05-10 11:16:19 +0100

    conact working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -51,6 +51,7 @@ export default function Contact(){
         headers: {
             "Content-Type": "application/json",
             "apikey": import.meta.env.VITE_SUPABASE_ANON_KEY,
+            "Authorization": `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
         },
         body: JSON.stringify({
             name: clientName,
